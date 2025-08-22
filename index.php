6d3f27b7916469a621628c5f22ef8e8847cbd365
Hary Sutrisno <hary.sutrisno@dhanisthasuryanusantara.com>
2025-08-22 15:25:50 +0700

    test: second test

--- index.php
+++ index.php
@@ -12,13 +12,11 @@ $botPage = $_ENV['BOT_PAGE'] ?? '/page-for-bot';
 $humanPage = $_ENV['HUMAN_PAGE'] ?? '/landing-page';
 $logDir = $_ENV['LOG_DIR'] ?? __DIR__ . '/logs/';
 
-var_dump($_SERVER);
-die;
-
 // --- Jalankan deteksi ---
 $detector = new BotDetector($_ENV['IPINFO_TOKEN'] ?? null);
 $result = $detector->analyzeRequest($_SERVER);
 
+var_dump($result);
 
 // --- Logging ---
 $logger = new TrafficLogger($logDir);
